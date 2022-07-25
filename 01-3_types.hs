5756507a909adac24a763d4ace8c930d5e74a884
Louise Swift <louise@louiseswift.com>
2022-07-25 09:00:02 +0100

    Begin 01-3_types.hs

---
+++ 01-3_types.hs
@@ -0,0 +1,12 @@
+-- exercise 1.6: what might the type definition for rem look like?
+
+-- rem :: Integer -> Integer -> Integer
+
+-- In fact, it is rem :: Integral a => a -> a -> a
+-- which is a "type scheme" meaning "if `a` is a type of class Integral,
+-- then `rem` is of type `a -> a -> a`
+
+-- `a` ranges over various types
+
+-- Integral is the class consisting of `Int` (fixed precision)
+-- and `Integer` (arbitray precision)
