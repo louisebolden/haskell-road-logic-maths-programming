98b91bd8b050c23dd5598fbffe0709f2d032b616
Louise Swift <louise@louiseswift.com>
2022-07-25 09:04:12 +0100

    01-4_identifiers.hs

---
+++ 01-4_identifiers.hs
@@ -0,0 +1,6 @@
+-- Variable identifiers: names of functions e.g. map
+
+-- Constructor identifiers: names of types e.g. True
+
+-- We build data structures out of constructors
+-- and operate on them with functions.
