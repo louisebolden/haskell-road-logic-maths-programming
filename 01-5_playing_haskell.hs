ebab7dea84a906fee07332fc26049447083dd307
Louise Swift <louise@louiseswift.com>
2022-07-27 08:17:18 +0100

    Additional exercises in 01-5_playing_haskell.hs

--- 01-5_playing_haskell.hs
+++ 01-5_playing_haskell.hs
@@ -12,8 +12,31 @@ mnmInt (x:xs) = min x (mnmInt xs)
 -- and call mnmInt with the new shorter list?
 -- otherwise, we'd never get out of this recursion!
 
+-- exercise 1.9: define a function that gives the maximum of a list of integers
+
+mxmInt :: [Int] -> Int
+mxmInt [] = error "empty list"
+mxmInt [x] = x
+mxmInt (x:xs) = max x (mxmInt xs)
+
 -- exercise 1.10: define a function removeFst that removes the first occurrence
 -- of an integer m from a list of integers. If m does not appear in the list,
 -- the list remains unchanged.
 
-removeFst :: [Int] -> [Int]
+-- first solution:
+
+removeFst :: Int -> [Int] -> [Int]
+removeFst m [] = error "empty list"
+removeFst m [x] | m == x = []
+                | otherwise = [x]
+removeFst m (x:xs) | m == x = xs
+                   | otherwise = x : (removeFst m xs)
+
+-- notes/corrections:
+-- 1. we don't need to error on empty list! instead, return the empty list as
+--    then we don't need to explicitly handle the cases where there is one
+--    element remaining in the list and it either matches `m` or it doesn't
+
+removeFst_ m [] = []
+removeFst_ m (x:xs) | m == x = xs
+                    | otherwise = x : (removeFst m xs)
