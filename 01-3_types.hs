a895a1b78098eb1cf7bb4567802f43452ab8141a
Louise Swift <louise@louiseswift.com>
2022-08-14 12:44:03 +0100

    Add to 01-3_types.hs

--- 01-3_types.hs
+++ 01-3_types.hs
@@ -2,6 +2,9 @@
 
 -- rem :: Integer -> Integer -> Integer
 
+rem 7 2
+-- => 1
+
 -- In fact, it is rem :: Integral a => a -> a -> a
 -- which is a "type scheme" meaning "if `a` is a type of class Integral,
 -- then `rem` is of type `a -> a -> a`
@@ -15,6 +18,15 @@
 
 -- Fractional is a class that contains the Rational type
 
+-- Rational is the type that results from applying the constructor Ratio to the
+-- tyoe Integer
+
+-- Float and Double represent floating point numbers (the elements of Double have
+-- higher precision)
+createFloat :: Float -> Float
+
 -- The type `[Char]` is abbreviated as `String` i.e. a string is a list of chars
 -- so we might have ['a', 'b', 'c'] or "abc" (notice the different quotes used)
 
+-- Pair- or tuple-formation: (a, b) denotes a pair with an object of type a and
+-- an object of type b; (a, b, c) denotes a triple, and so on
