ba78a4785fb4aada14e83e5581aefe9586de4388
Louise Swift <louise@louiseswift.com>
2022-08-19 11:37:32 +0100

    Add to 01-3_types.hs

--- 01-3_types.hs
+++ 01-3_types.hs
@@ -2,7 +2,7 @@
 
 -- rem :: Integer -> Integer -> Integer
 
-rem 7 2
+result = rem 7 2
 -- => 1
 
 -- In fact, it is rem :: Integral a => a -> a -> a
@@ -23,10 +23,69 @@ rem 7 2
 
 -- Float and Double represent floating point numbers (the elements of Double have
 -- higher precision)
-createFloat :: Float -> Float
 
 -- The type `[Char]` is abbreviated as `String` i.e. a string is a list of chars
 -- so we might have ['a', 'b', 'c'] or "abc" (notice the different quotes used)
 
 -- Pair- or tuple-formation: (a, b) denotes a pair with an object of type a and
 -- an object of type b; (a, b, c) denotes a triple, and so on
+
+-- exercise 1.18: find expressions with the following types:
+-- 1. [String]
+
+answer1 = ["abc"]
+
+-- 2. (Bool, String)
+
+answer2 = (True, "abc")
+
+-- 3. [(Bool, String)]
+
+answer3 = [(False, "xyz")]
+
+-- 4. ([Bool], String)
+
+answer4 = ([True], "xyz")
+
+-- 5. Bool -> Bool
+
+negation :: Bool -> Bool
+negation bool | bool == True = False
+              | bool == False = True
+
+-- exercise 1.19: use the interpreter command `:t` to find the types of the
+-- following predefined functions:
+-- 1. head
+
+head :: [a] -> a
+-- e.g. head [2,3,1] => 2
+
+-- 2. last
+
+last :: [a] -> a
+-- e.g. last [2,3,1] => 1
+
+-- 3. init
+
+init :: [a] -> [a]
+-- e.g. init [2,3,1,4,9] => [2,3,1,4]
+
+-- 4. fst
+
+fst :: (a, b) -> a
+-- e.g. fst (2,1) => 2
+
+-- 5. (++)
+
+(++) :: [a] -> [a] -> [a]
+-- e.g. [1] ++ [2] => [1,2]
+
+-- 6. flip
+
+flip :: (a -> b -> c) -> b -> a -> c
+-- e.g. flip (>) 3 5 => true
+
+-- 7. flip (++)
+
+flip (++) :: [a] -> [a] -> [a]
+-- e.g. flip (++) [2] [1] => [1,2]
